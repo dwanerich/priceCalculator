b22103700b7c1b5931df0f6ee2fae8d149a6bada
dwanerich <dwanerich@gmail.com>
2021-09-24 22:02:48 -0400

    created an inventory object to store items, unit prices & sale prices

---
+++ priceCalculator.js
@@ -0,0 +1,33 @@
+let Inventory = {
+    Milk: {
+        unit_price: 3.97,
+        sale_price: 5.00
+    },
+    Bread: {
+        unit_price: 2.17,
+        sale_price: 6.00
+    },
+    Banana: {
+        unit_price: .99
+    },
+    Apple: {
+        unit_price: .89
+    }
+}
+
+// class Item {
+//     constructor(name, quantity = 0, price) {
+//         this.name = name;
+//         this.quantity = quantity;
+//         this.price = price;
+//     }
+
+// }
+
+// let totalPrice = (name, quantity, price) => {
+//     return "Total Price: $123"
+// }
+
+// let youSaved = (name, quantity, price) => {
+//     return "You Saved xyz Today"
+// }
