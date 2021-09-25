c73c16e21abaab64e0d99c7355f25f0c69539cb8
dwanerich <dwanerich@gmail.com>
2021-09-24 23:07:04 -0400

    created an object variable for the checkOut items with name & quantity key value pairs

---
+++ inventory.js
@@ -0,0 +1,17 @@
+Inventory =
+    {
+    "Milk": {
+        "unit_price": 3.97,
+        "sale_price": 5.00
+    },
+    "Bread": {
+        "unit_price": 2.17,
+        "sale_price": 6.00
+    },
+    "Banana": {
+        "unit_price": .99
+    },
+    "Apple": {
+        "unit_price": .89
+    }
+}
--- priceCalculator.js
+++ priceCalculator.js
@@ -1,32 +1,24 @@
-let Inventory = {
-    Milk: {
-        unit_price: 3.97,
-        sale_price: 5.00
-    },
-    Bread: {
-        unit_price: 2.17,
-        sale_price: 6.00
-    },
-    Banana: {
-        unit_price: .99
-    },
-    Apple: {
-        unit_price: .89
-    }
+Inventory =
+{
+    "Milk": {"unit_price": 3.97, "sale_price": 5.00},
+    "Bread": {"unit_price": 2.17, "sale_price": 6.00},
+    "Apple": {"unit_price": .89},
+    "Banana": {"unit_price": .99}
 }
 
-class Item {
-    constructor(name, quantity = 0, price) {
-        this.name = name;
-        this.quantity = quantity;
-        this.price = price;
-    }
+checkOut = { "Milk": 3, "Bread": 4, "Apple": 1, "Banana": 1 }
+
+
+let totalItems = () => {
+    return Object.keys(checkOut).length;
 }
 
-let totalPrice = (name, quantity, price) => {
-    return "Total Price: $123"
-}
+// console.log(Inventory["Milk"])
+console.log(totalItems())
+
 
 // let youSaved = (name, quantity, price) => {
 //     return "You Saved xyz Today"
 // }
+
+
