55f6ca856f34162bcb7f011eafa2731c57fa229e
Dwane Richards <49284432+dwanerich@users.noreply.github.com>
2021-09-28 11:38:42 -0400

    Update itemClass.js

--- itemClass.js
+++ itemClass.js
@@ -1,7 +1,6 @@
 class Item {
     constructor(name, unit_price, sale_price) {
         this.name = name;
-        this.quantity
         this.unit_price = unit_price;
         this.sale_price = sale_price
     }   
